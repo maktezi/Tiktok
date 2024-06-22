c4bce113f96356d2a0d281eab865ead652bd9d72
maktezi <mlcnierras@gmail.com>
2024-06-22 18:49:29 +0800

    chore(backend): update Laravel dependencies and services

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -6,6 +6,13 @@
       0 => 'Laravel\\Sail\\SailServiceProvider',
     ),
   ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
   'laravel/tinker' => 
   array (
     'providers' => 
@@ -32,6 +39,17 @@
     'providers' => 
     array (
       0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
     ),
   ),
 );
--- client/components/auth/Login.vue
+++ client/components/auth/Login.vue
@@ -1,42 +1,56 @@
 <template>
-  <div class="text-center text-[28px] mb-4 font-bold text-black">Login</div>
+    <div class="text-center text-[28px] mb-4 font-bold text-black">Login</div>
 
-  <div class="px-6 pb-1.5 text-[15px] text-black">Email address</div>
+    <div class="px-6 pb-1.5 text-[15px] text-black">Email address</div>
 
-  <div class="px-6 pb-2">
-    <TextInput
-      placeholder="Email address"
-      v-model:input="email"
-      inputType="email"
-      :autofocus="true"
-      error=""
-    />
-  </div>
-  <div class="px-6 pb-2">
-    <TextInput
-      placeholder="Password"
-      v-model:input="password"
-      inputType="password"
-      error=""
-    />
-  </div>
+    <div class="px-6 pb-2">
+        <TextInput
+            placeholder="Email address"
+            v-model:input="email"
+            inputType="email"
+            :autofocus="true"
+            error=""
+        />
+    </div>
+    <div class="px-6 pb-2">
+        <TextInput
+            placeholder="Password"
+            v-model:input="password"
+            inputType="password"
+            error=""
+        />
+    </div>
     <div class="px-6 text-[12px] text-gray-600">Forgot password?</div>
 
     <div class="px-6 pb-2 mt-6">
-      <button
-        :disabled="(!email || !password)"
-        :class="(!email || !password ? 'bg-gray-200' : 'bg-[#F02C56]')"
-        @click="() => login()"
-        class="w-full text-[17px] font-semibold text-white py-3 rounded-sm"
-      >
-        Log in
-      </button>
+        <!--        :disabled="!email || !password"-->
+        <!--        :class="!email || !password ? 'bg-gray-200' : 'bg-[#F02C56]'"-->
+        <button
+            @click="() => login()"
+            class="w-full text-[17px] font-semibold bg-[#F02C56] text-white py-3 rounded-sm"
+        >
+            Log in
+        </button>
     </div>
-
 </template>
 
 <script setup lang="ts">
-let email = ref('')
-let password = ref('')
-let errors = ref('')
+const { $axios } = useNuxtApp();
+let email = ref("");
+let password = ref("");
+let errors = ref("");
+
+const login = async () => {
+    try {
+        await $axios.get("/sanctum/csrf-cookie");
+        await $axios.post("./login", {
+            email: "admin@mail.com",
+            password: "admin1234",
+        });
+        let res = await $axios.get("/api/user");
+        console.log(res);
+    } catch (er) {
+        console.log(er);
+    }
+};
 </script>
